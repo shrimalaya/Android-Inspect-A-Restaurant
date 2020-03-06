e4c266010be185e79be6db91ada1cdea4f546be9
Tony Shen <tsa87@sfu.ca>
2020-03-06 11:01:18 -0800

    Added fields of inspection class

--- app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Inspection.java
+++ app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Inspection.java
@@ -1,6 +1,10 @@
 package com.example.cmpt_cobalt.ca.cmpt276A3.model;
 
 public class Inspection {
-    //TODO: 1. List of violations
-    //      2.
+    private String trackingNumber;
+    private String inspectionDate;
+    private String inspectionType;
+    private int numCritical;
+    private int numNonCritical;
+    private String hazardRating;
 }
