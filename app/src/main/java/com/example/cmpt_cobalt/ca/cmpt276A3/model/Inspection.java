c44376ae813618ecc599a3dfcb2919b6729a7c6d
Tony Shen <tsa87@sfu.ca>
2020-03-06 11:10:27 -0800

    Inspection Class done

--- app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Inspection.java
+++ app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Inspection.java
@@ -7,4 +7,71 @@ public class Inspection {
     private int numCritical;
     private int numNonCritical;
     private String hazardRating;
+    private String violations;
+
+    public Inspection(String trackingNumber, String inspectionDate, String inspectionType, int numCritical, int numNonCritical, String hazardRating, String violations) {
+        this.trackingNumber = trackingNumber;
+        this.inspectionDate = inspectionDate;
+        this.inspectionType = inspectionType;
+        this.numCritical = numCritical;
+        this.numNonCritical = numNonCritical;
+        this.hazardRating = hazardRating;
+        this.violations = violations;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getInspectionDate() {
+        return inspectionDate;
+    }
+
+    public void setInspectionDate(String inspectionDate) {
+        this.inspectionDate = inspectionDate;
+    }
+
+    public String getInspectionType() {
+        return inspectionType;
+    }
+
+    public void setInspectionType(String inspectionType) {
+        this.inspectionType = inspectionType;
+    }
+
+    public int getNumCritical() {
+        return numCritical;
+    }
+
+    public void setNumCritical(int numCritical) {
+        this.numCritical = numCritical;
+    }
+
+    public int getNumNonCritical() {
+        return numNonCritical;
+    }
+
+    public void setNumNonCritical(int numNonCritical) {
+        this.numNonCritical = numNonCritical;
+    }
+
+    public String getHazardRating() {
+        return hazardRating;
+    }
+
+    public void setHazardRating(String hazardRating) {
+        this.hazardRating = hazardRating;
+    }
+
+    public String getViolations() {
+        return this.violations;
+    }
+
+    public void setViolations(String violations) {
+        this.violations = violations;
+    }
 }
