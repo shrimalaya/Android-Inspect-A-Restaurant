ff306979908d8250b011347c34a3f58e6d3b3e23
akash <asd15@sfu.ca>
2020-03-08 00:21:25 -0800

    [ADhami] Added toString for returning trackID and Restaurant Name

--- app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Restaurant.java
+++ app/src/main/java/com/example/cmpt_cobalt/ca/cmpt276A3/model/Restaurant.java
@@ -1,5 +1,7 @@
 package com.example.cmpt_cobalt.ca.cmpt276A3.model;
 
+import androidx.annotation.NonNull;
+
 import java.util.ArrayList;
 
 public class Restaurant {
@@ -92,6 +94,12 @@ public class Restaurant {
     public Inspection getInspection(int inspection) {
         return inspections.get(inspection);
 
+    }
+
+    @Override
+    public String toString() {
+        return tracking + ' '
+                + name;
     }
 
     /* TODO: 1. Address
